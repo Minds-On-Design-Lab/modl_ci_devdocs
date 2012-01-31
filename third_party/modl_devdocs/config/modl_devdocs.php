a61cb24acfd03a7552945410b279d1f2a2dd3732
Mike Joseph <josephm5@msu.edu>
2012-01-30 20:11:50 -0500

    added comments

--- third_party/modl_devdocs/config/modl_devdocs.php
+++ third_party/modl_devdocs/config/modl_devdocs.php
@@ -2,12 +2,18 @@
 
 $config = array(
 
-	'version'		=> '1.0',
-	'build'			=> '20120130',
-
+	// The DevDocs package can store cached versions of your docs for
+	// faster loading of large, complicated documentation.
 	'enable_cache'	=> true,
 
+	// DevDocs can generate a table of contents based on your header structure
 	'auto_toc'		=> true,
 
+	// The default name for the documentation file
 	'default_name'	=> 'devdocs',
+
+	// please don't edit these
+
+	'version'		=> '1.0',
+	'build'			=> '20120130',
 );
